3fc66adc9776fd99956f2f8c6e2a07e1575e49ea
ferinofab <dsfsdfds@.com>
2024-12-10 13:37:28 +0700

    debag2

--- handler/Add_admission.php
+++ handler/Add_admission.php
@@ -3,17 +3,18 @@ $pdo = require $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 ///store
 $date_time = $_POST['date_time'];
 $amount = $_POST['amount'];
-echo $amount;
 $id =$_POST['id']??null;
 
 if($id == null){
     $select = $_POST['select'];
-    $pdo->query("insert into characteristics (date_time, product_id, amount) values ('$date_time', '$select', '$amount')");
     $pdo->query("update product set ammount_product = ammount_product + '$amount' where id = '$select' ");
+    $pdo->query("insert into characteristics (date_time, product_id, amount) values ('$date_time', '$select', '$amount')");
+
 
 }else{
-    $pdo->query("insert into characteristics (date_time, product_id, amount) values ('$date_time', '$id', '$amount')");
     $pdo->query("update product set ammount_product = ammount_product + '$amount' where id = '$id' ");
+    $pdo->query("insert into characteristics (date_time, product_id, amount) values ('$date_time', '$id', '$amount')");
 
 }
+header("Locatiom: /index.php");
 
